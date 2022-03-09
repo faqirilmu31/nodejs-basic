993453efd07410101fd62fe9360f99017a8ef0c7
Just an ordinary girl <renisetyaningsih2931@gmail.com>
2022-03-10 00:47:08 +0700

    add response status

--- server.js
+++ server.js
@@ -38,20 +38,22 @@ const requestListener = (request, response) => {
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
 
     const { method, url } = request;
 
     if(url === '/') {
         // logika respons bila url bernilai '/'
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if(url === '/about') {
         // logika respons bila url bernilai '/about'
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if(method === 'POST') {
             let body = [];
@@ -61,15 +63,18 @@ const requestListener = (request, response) => {
             });
 
             request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const {name} = JSON.parse(body);
-            response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
         // logika respons bila url bukan '/' atau '/about'
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
